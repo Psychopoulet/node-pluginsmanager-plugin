3821e7af8cc13d08f0367bfe69be3e926f680147
Sébastien Vidal <svidal1@free.fr>
2020-01-21 16:21:51 +0100

    improve descriptor tests

---
+++ test/utils/Orchestrator/generateDescriptorWithPaths.js
@@ -0,0 +1,40 @@
+"use strict";
+
+// deps
+
+	// natives
+	const { join } = require("path");
+	const { writeFile } = require("fs");
+
+	// locals
+	const readJSONFile = require(join(__dirname, "..", "..", "..", "lib", "utils", "readJSONFile.js"));
+
+// consts
+
+	const DESCRIPTOR = join(__dirname, "..", "DescriptorUser", "Descriptor.json");
+
+// module
+
+// module
+
+module.exports = function generateTmpDescriptorWithPaths (paths, targetedFile, logs = false) {
+
+	return readJSONFile(DESCRIPTOR).then((descriptor) => {
+
+		return new Promise((resolve, reject) => {
+
+			descriptor.paths = paths;
+
+			if (logs) {
+				(0, console).log(JSON.stringify(descriptor));
+			}
+
+			writeFile(targetedFile, JSON.stringify(descriptor), "utf8", (err) => {
+				return err ? reject(err) : resolve();
+			});
+
+		});
+
+	});
+
+};
