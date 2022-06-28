3d86b0bcb8baad21f83627b220d4fd535dc84e40
Seb - physical <svidal1@free.fr>
2022-06-28 16:04:39 +0200

    improve utils typings

--- src/utils/descriptor/extractParams.ts
+++ src/utils/descriptor/extractParams.ts
@@ -11,9 +11,9 @@ export default function extractParams (patternPath: string, realPath: string): {
 
 	const params: { [key:string]: any } = {};
 
-		const patternPathSplitted = removeFirstSlash(patternPath).split("/");
+		const patternPathSplitted: Array<string> = removeFirstSlash(patternPath).split("/");
 
-		removeFirstSlash(realPath).split("/").forEach((p, i) => {
+		removeFirstSlash(realPath).split("/").forEach((p: string, i: number): void => {
 
 			if ("{" === patternPathSplitted[i][0]) {
 				params[patternPathSplitted[i].replace("{", "").replace("}", "")] = decodeURI(p);
