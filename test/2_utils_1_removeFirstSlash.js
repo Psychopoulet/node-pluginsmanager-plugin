2157b1767dcc88ab0d19e247fb701b4d548e7a30
Seb - physical <svidal1@free.fr>
2022-06-29 18:22:56 +0200

    improve utils tests

--- test/2_utils_1_removeFirstSlash.js
+++ test/2_utils_1_removeFirstSlash.js
@@ -1,7 +1,3 @@
-/*
-	eslint-disable no-undefined
-*/
-
 "use strict";
 
 // deps
@@ -11,34 +7,34 @@
 	const { join } = require("path");
 
 	// locals
-	const removeFirstSlash = require(join(__dirname, "..", "lib", "utils", "removeFirstSlash.js"));
+	const removeFirstSlash = require(join(__dirname, "..", "lib", "cjs", "utils", "removeFirstSlash.js"));
 
 // tests
 
 describe("utils / removeFirstSlash", () => {
 
 	it("should test with missing data", () => {
-		strictEqual(removeFirstSlash(), "undefined", "generated data is not as expected");
+		strictEqual(removeFirstSlash.default(), "undefined", "generated data is not as expected");
 	});
 
 	it("should test with wrong data", () => {
-		strictEqual(removeFirstSlash(false), "false", "generated data is not as expected");
+		strictEqual(removeFirstSlash.default(false), "false", "generated data is not as expected");
 	});
 
 	it("should test with empty data", () => {
-		strictEqual(removeFirstSlash(""), "", "generated data is not as expected");
+		strictEqual(removeFirstSlash.default(""), "", "generated data is not as expected");
 	});
 
 	it("should test with string data", () => {
-		strictEqual(removeFirstSlash("test"), "test", "generated data is not as expected");
+		strictEqual(removeFirstSlash.default("test"), "test", "generated data is not as expected");
 	});
 
 	it("should test with path data", () => {
-		strictEqual(removeFirstSlash("/test/test"), "test/test", "generated data is not as expected");
+		strictEqual(removeFirstSlash.default("/test/test"), "test/test", "generated data is not as expected");
 	});
 
 	it("should test with path data without first slash", () => {
-		strictEqual(removeFirstSlash("test/test"), "test/test", "generated data is not as expected");
+		strictEqual(removeFirstSlash.default("test/test"), "test/test", "generated data is not as expected");
 	});
 
 });
