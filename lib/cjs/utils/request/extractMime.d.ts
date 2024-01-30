edb9cd24b477c727eee5873b2df974444c9e0470
Sébastien VIDAL <meona@live.fr>
2024-01-30 10:11:02 +0100

    improve lint

--- lib/cjs/utils/request/extractMime.d.ts
+++ lib/cjs/utils/request/extractMime.d.ts
@@ -1,6 +1,4 @@
 export default function extractMime(contentType: string, code: number, responses: Record<string, {
     "description": string;
-    "content"?: {
-        [key: string]: any;
-    };
+    "content"?: Record<string, any>;
 }>): string;
--- src/utils/request/extractMime.ts
+++ src/utils/request/extractMime.ts
@@ -13,9 +13,7 @@
 
 export default function extractMime (contentType: string, code: number, responses: Record<string, {
     "description": string;
-    "content"?: {
-        [key:string]: any
-    }
+    "content"?: Record<string, any>
 }>): string {
 
     const err = checkStringSync("contentType", contentType)
