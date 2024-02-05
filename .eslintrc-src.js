53194a707ed07ad3c71110cf857fc811d36dd10f
Sébastien <svidal1@free.fr>
2024-02-05 17:45:28 +0100

    improve lint rules

--- .eslintrc-src.js
+++ .eslintrc-src.js
@@ -1,8 +1,19 @@
 module.exports = {
+
     "extends": [ "plugin:eslint-plugin-personnallinter/ts-back" ],
+
     "rules": {
+
+        // specific to this package
         "n/no-sync": "off",
-        "@typescript-eslint/no-explicit-any": "warn",
-        "@typescript-eslint/explicit-module-boundary-types": "warn"
+
+        // API stuff
+        "@typescript-eslint/no-explicit-any": "off",
+        "@typescript-eslint/no-unsafe-argument": "off",
+        "@typescript-eslint/no-unsafe-assignment": "off",
+        "@typescript-eslint/no-unsafe-member-access": "off",
+        "@typescript-eslint/no-unsafe-return": "off"
+
     }
+
 };
