8dc3f1b9205619599b41d8a7291d8024eedc424d
Sébastien VIDAL <sebastien.vidal.dev@gmail.com>
2026-02-16 19:14:41 +0100

    add import tests

---
+++ test/0_main_exports.js
@@ -0,0 +1,91 @@
+"use strict";
+
+// deps
+
+	// natives
+	const { join } = require("path");
+	const { strictEqual } = require("assert");
+
+	// locals
+	const main = require(join(__dirname, "..", "lib", "cjs", "main.cjs"));
+
+// consts
+
+	const CHECKER_NAMES = [
+		"checkExists", "checkExistsSync",
+		"checkBoolean", "checkBooleanSync",
+		"checkFunction", "checkFunctionSync",
+		"checkNumber", "checkNumberSync",
+		"checkObject", "checkObjectSync",
+		"checkString", "checkStringSync",
+		"checkArray", "checkArraySync",
+		"checkInteger", "checkIntegerSync",
+		"checkNonEmptyArray", "checkNonEmptyArraySync",
+		"checkNonEmptyInteger", "checkNonEmptyIntegerSync",
+		"checkNonEmptyNumber", "checkNonEmptyNumberSync",
+		"checkNonEmptyObject", "checkNonEmptyObjectSync",
+		"checkNonEmptyString", "checkNonEmptyStringSync",
+		"checkNumberBetween", "checkNumberBetweenSync",
+		"checkObjectLength", "checkObjectLengthSync",
+		"checkObjectLengthBetween", "checkObjectLengthBetweenSync",
+		"checkStringLength", "checkStringLengthSync",
+		"checkStringLengthBetween", "checkStringLengthBetweenSync",
+		"checkIntegerBetween", "checkIntegerBetweenSync",
+		"checkArrayLength", "checkArrayLengthSync",
+		"checkArrayLengthBetween", "checkArrayLengthBetweenSync"
+	];
+
+	const COMPONENT_NAMES = [
+		"DescriptorUser",
+		"Mediator",
+		"MediatorUser",
+		"Orchestrator",
+		"Server"
+	];
+
+	const ERROR_NAMES = [
+		"UnauthorizedError",
+		"NotFoundError",
+		"LockedError"
+	];
+
+// tests
+
+describe("main entry exports", () => {
+
+	it("should export an object", () => {
+		strictEqual(typeof main, "object", "main must be an object");
+		strictEqual(main !== null, true, "main must not be null");
+	});
+
+	it("should export all checker functions", () => {
+		for (const name of CHECKER_NAMES) {
+			strictEqual(typeof main[name], "function", "export \"" + name + "\" must be a function");
+		}
+	});
+
+	it("should export all component classes", () => {
+		for (const name of COMPONENT_NAMES) {
+			strictEqual(typeof main[name], "function", "export \"" + name + "\" must be a class (function)");
+		}
+	});
+
+	it("should export all error classes", () => {
+		for (const name of ERROR_NAMES) {
+			strictEqual(typeof main[name], "function", "export \"" + name + "\" must be a class (function)");
+		}
+	});
+
+	it("should have exactly the expected exports (no missing or extra)", () => {
+		const expected = new Set([...CHECKER_NAMES, ...COMPONENT_NAMES, ...ERROR_NAMES]);
+		const actual = new Set(Object.keys(main).filter((k) => { return "default" !== k && !k.startsWith("__"); }));
+		strictEqual(actual.size, expected.size, "export count must match expected");
+		for (const key of expected) {
+			strictEqual(actual.has(key), true, "missing export \"" + key + "\"");
+		}
+		for (const key of actual) {
+			strictEqual(expected.has(key), true, "unexpected export \"" + key + "\"");
+		}
+	});
+
+});
