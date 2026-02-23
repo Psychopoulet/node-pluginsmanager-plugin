b4acefb95f7585656f07bf477709c18bc11ce1d2
Sébastien VIDAL <sebastien.vidal.dev@gmail.com>
2026-02-23 13:22:38 +0100

    improve lint

--- lib/src/utils/send.ts
+++ lib/src/utils/send.ts
@@ -67,40 +67,41 @@ export default function send (req: IncomingMessage, res: iServerResponse, code: 
 
             res.statusCode = code;
 
-            res.headers = Object.assign({
+            res.headers = {
                 "Content-Type": options.mime,
                 "Content-Length": 0 < formattedContent.length ? Buffer.byteLength(formattedContent) : 0,
                 "Status-Code-Url-Cat": "https://http.cat/" + code,
-                "API-Version": options.apiVersion
-            }, options.cors ? {
-                "Access-Control-Allow-Origin": "*",
-                "Access-Control-Allow-Credentials": true,
-                "Access-Control-Allow-Methods": req.headers["access-control-request-method"]
-                    ? req.headers["access-control-request-method"] as string
-                    : [
-                        "GET",
-                        "POST",
-                        "PUT",
-                        "DELETE",
-                        "PATCH",
-                        "OPTIONS"
-                    ].join(", "),
-                "Access-Control-Allow-Headers": req.headers["access-control-request-headers"]
-                    ? req.headers["access-control-request-headers"] as string
-                    : [
-                        "Origin",
-                        "Accept",
-                        "Accept-Version",
-                        "Content-Length",
-                        "Content-MD5",
-                        "Content-Type",
-                        "Date",
-                        "X-Api-Version",
-                        "X-File-Name",
-                        "X-CSRF-Token",
-                        "X-Requested-With"
-                    ].join(", ")
-            } : {});
+                "API-Version": options.apiVersion,
+                ...(options.cors ? {
+                    "Access-Control-Allow-Origin": "*",
+                    "Access-Control-Allow-Credentials": true,
+                    "Access-Control-Allow-Methods": req.headers["access-control-request-method"]
+                        ? req.headers["access-control-request-method"] as string
+                        : [
+                            "GET",
+                            "POST",
+                            "PUT",
+                            "DELETE",
+                            "PATCH",
+                            "OPTIONS"
+                        ].join(", "),
+                    "Access-Control-Allow-Headers": req.headers["access-control-request-headers"]
+                        ? req.headers["access-control-request-headers"] as string
+                        : [
+                            "Origin",
+                            "Accept",
+                            "Accept-Version",
+                            "Content-Length",
+                            "Content-MD5",
+                            "Content-Type",
+                            "Date",
+                            "X-Api-Version",
+                            "X-File-Name",
+                            "X-CSRF-Token",
+                            "X-Requested-With"
+                        ].join(", ")
+                } : {})
+            };
 
             // send data
 
