7e2d6015b140b7d15e7a043be8c85c6b28f466c5
Sébastien VIDAL <sebastien.vidal.dev@gmail.com>
2026-03-06 16:48:31 +0100

    remove linter limitations

--- .eslintrc-back.js
+++ .eslintrc-back.js
@@ -16,14 +16,7 @@ module.exports = defineConfig({
     "rules": {
 
         // specific to this package
-        "n/no-sync": "off",
-
-        // API stuff
-        "@typescript-eslint/no-explicit-any": "off",
-        "@typescript-eslint/no-unsafe-argument": "off",
-        "@typescript-eslint/no-unsafe-assignment": "off",
-        "@typescript-eslint/no-unsafe-member-access": "off",
-        "@typescript-eslint/no-unsafe-return": "off"
+        "n/no-sync": "warn"
 
     }
 
